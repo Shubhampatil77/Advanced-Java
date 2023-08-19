bb3232e4a27c844b506edc4570d75a116f06fce8
Shubham <126310585+Shubhampatil77@users.noreply.github.com>
2023-08-19 08:36:21 +0530

    Add files via upload

---
+++ cill.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.bgcolor("white")
+
+squary = turtle.Turtle()
+squary.speed(600)
+squary.pencolor("red")
+for i in range(600):
+    squary.forward(i)
+    squary.left(135) 
+    
