bf206149d23f8199e0397cfae6f2e347b45b9cd9
Shubham <126310585+Shubhampatil77@users.noreply.github.com>
2023-08-19 08:35:34 +0530

    Add files via upload

--- Demo2.java
+++ Demo2.java
@@ -1,14 +1,9 @@
-package Exception;
-import java.util.*;
 
-public class Demo2
-{
-	public static void main(String[] args)throws Exception	
-	{
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter a character: ");
-		char c = sc.nextLine().charAt(0);
-		System.out.println("Character is: " + c);
+public class Demo2 {
 
-		}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
